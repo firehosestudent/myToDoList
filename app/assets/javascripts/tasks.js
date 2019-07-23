fb0383498d9945a1395ebb2d98769431c61a5217
jonny phun <jonny_phun@hotmail.com>
2019-07-23 05:03:06 +0000

    finishing touches

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,13 +5,11 @@ $(function() {
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
     var liClass = task.done ? "completed" : "";
-    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
-    '<div class="view"><input class="toggle" type="checkbox"' +
-      " data-id='" + task.id + "'" +
+    var liElement = '<li id="listItem-' + task.id + '" class="' + liClass + '"><div class="view"><input class="toggle" type="checkbox" data-id="' + task.id + '"' +
       checkedStatus +
       '><label>' +
        task.title +
-       '</label></div></li>';
+       '</label><button class="destroy"></button></div></li>';
 
     return liElement;
   }
@@ -30,15 +28,40 @@ $(function() {
       task: {
         done: doneValue
       }
-    }).success(function(data) {
-      var liHtml = taskHtml(data);
+    }).success( function( data ) {
+      var liHtml = taskHtml( data );
       var $li = $("#listItem-" + data.id);
       $li.replaceWith(liHtml);
       $('.toggle').change(toggleTask);
-
-      
-    });
+      var liCount = $('.todo-list li').length;
+      renderFooter(liCount);
+    } );
   }
+
+
+  // renders footer
+  function renderFooter(liCount) {
+    // calculate li elements without class .completed (so, active elements)
+    var liActive = ($('li').length) - ($('.completed').length);
+
+    // builds a var to store the ENTIRE block of .footer code
+    if (liActive === 1) {
+      var footerHtmlString = '<footer class="footer"><span id="todo-count"><strong>1</strong> item left</span></footer>';
+    } else {
+      var footerHtmlString = '<footer class="footer"><span id="todo-count"><strong>' + liActive + '</strong> items left</span></footer>';
+    }
+    
+    // grabs the footer element
+    var footerBlock = $('footer');
+
+    // replaces empty footer tags with footerHtmlString code
+    // IF there is at least 1 li
+    if (liCount > 0) {
+      footerBlock.html(footerHtmlString);
+    }
+
+  }
+
 
   $.get("/tasks").success( function( data ) {
     var htmlString = "";
@@ -48,6 +71,10 @@ $(function() {
     });
     var ulTodos = $('.todo-list');
     ulTodos.html(htmlString);
+
+    var liCount = $('.todo-list li').length;
+
+    renderFooter(liCount);
 
     $('.toggle').change(toggleTask);
 
@@ -62,11 +89,35 @@ $(function() {
         title: textbox.val()
       }
     };
-    $.post("/tasks", payload).success(function(data) {
+    $.post("/tasks", payload).success( function( data ) {
       var htmlString = taskHtml(data);
       var ulTodos = $('.todo-list');
       ulTodos.append(htmlString);
       $('.toggle').click(toggleTask);
+      $('.new-todo').val('');
+      var liCount = $('.todo-list li').length;
+      renderFooter(liCount);
+    });
+  });
+
+  // destroy task
+  $('.todo-list').on('click', '.destroy', function() {
+    var itemId = $(this).siblings('input').data('id');
+    var liId = $(this).closest('li')
+    $.ajax({
+      url: "/tasks/" + itemId,
+      type: 'DELETE',
+      success: function(result) {
+        // Don't need to do something *with the result*, but need to do something
+        liId.remove();
+        // counts li elements
+        var liCount = $('.todo-list li').length;
+        if (liCount === 0) {
+          $('.footer').remove();
+        } else {
+          renderFooter(liCount);
+        }
+      }
     });
   });
 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,12 @@ class TasksController < ApplicationController
     render json: task
   end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    render json: {"response" => :success}
+  end
+
   private
 
   def task_params
